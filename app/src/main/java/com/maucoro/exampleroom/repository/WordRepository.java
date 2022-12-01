3d839a5a8e59294e927fb191fcf5c1417e1e07ff
Mauricio Coronado <mauricio.coronado08@gmail.com>
2022-12-01 09:25:49 -0500

    add viewModel word

--- app/src/main/java/com/maucoro/exampleroom/repository/WordRepository.java
+++ app/src/main/java/com/maucoro/exampleroom/repository/WordRepository.java
@@ -10,7 +10,7 @@ import com.maucoro.exampleroom.entity.Word;
 
 import java.util.List;
 
-class WordRepository {
+public class WordRepository {
 
     private WordDao mWordDao;
     private LiveData<List<Word>> mAllWords;
@@ -19,7 +19,7 @@ class WordRepository {
     // dependency. This adds complexity and much more code, and this sample is not about testing.
     // See the BasicSample in the android-architecture-components repository at
     // https://github.com/googlesamples
-    WordRepository(Application application) {
+    public WordRepository(Application application) {
         WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
         mWordDao = db.wordDao();
         mAllWords = mWordDao.getAlphabetizedWords();
@@ -27,13 +27,13 @@ class WordRepository {
 
     // Room executes all queries on a separate thread.
     // Observed LiveData will notify the observer when the data has changed.
-    LiveData<List<Word>> getAllWords() {
+    public LiveData<List<Word>> getAllWords() {
         return mAllWords;
     }
 
     // You must call this on a non-UI thread or your app will throw an exception. Room ensures
     // that you're not doing any long running operations on the main thread, blocking the UI.
-    void insert(Word word) {
+    public void insert(Word word) {
         WordRoomDatabase.databaseWriteExecutor.execute(() -> {
             mWordDao.insert(word);
         });
