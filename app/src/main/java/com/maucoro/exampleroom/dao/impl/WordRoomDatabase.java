207aeb3a8a183505b3a162ca585351bfbc8a64b4
Mauricio Coronado <mauricio.coronado08@gmail.com>
2022-12-01 10:11:29 -0500

    Populate the database

--- app/src/main/java/com/maucoro/exampleroom/dao/impl/WordRoomDatabase.java
+++ app/src/main/java/com/maucoro/exampleroom/dao/impl/WordRoomDatabase.java
@@ -2,9 +2,11 @@ package com.maucoro.exampleroom.dao.impl;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.maucoro.exampleroom.dao.WordDao;
 import com.maucoro.exampleroom.entity.Word;
@@ -28,10 +30,34 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                                     WordRoomDatabase.class, "word_database")
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
     }
+
+    /**
+     * Override the onCreate method to populate the database.
+     * For this sample, we clear the database every time it is created.
+     */
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            databaseWriteExecutor.execute(() -> {
+                // Populate the database in the background.
+                // If you want to start with more words, just add them.
+                WordDao dao = INSTANCE.wordDao();
+                dao.deleteAll();
+
+                Word word = new Word("Hello");
+                dao.insert(word);
+                word = new Word("World");
+                dao.insert(word);
+            });
+        }
+    };
 }
